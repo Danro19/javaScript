919f129776b2ca3f5b3ca90539cd21ddacc6e1bd
Danro19 <danrodri1911@gmail.com>
2024-12-05 21:12:51 -0500

    AÑADIR CLASE 5-12

---
+++ sesion8-05-12/ejercicios/ejercicio1/script.js
@@ -0,0 +1,26 @@
+    const doTask = (iterations) => {
+    const numbers = [];
+    for (let i = 0; i < iterations; i++) {
+        const number = 1 + Math.floor(Math.random() * 6);
+        numbers.push(number);
+        
+        if (number === 6) {
+            setTimeout(async () => {
+            console.log(await "iteraciones necesarias:", i + 1);
+            }, 2000);
+        return {
+            error: true,
+            
+            message: "Se ha sacado un 6",
+           
+        };
+        }
+    }
+    return {
+        error: false,
+        value: numbers,
+    };
+    
+    };
+    console.log(doTask(10));
+
